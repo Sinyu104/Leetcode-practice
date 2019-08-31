c25435c033e699dcfa2712bb5dcc2e4bb813f54f
Sinyu104 <brandyhuang104@gmail.com>
2019-08-31 20:04:40 +0800

    Finishing the first question.

--- 2-two_sum/solution.cpp
+++ 2-two_sum/solution.cpp
@@ -1,24 +1,26 @@
 #include "iostream"
 #include <vector>
-#include <unordered_map>
+#include <map>
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> record;
-
-        for (int i = 0; i != nums.size(); i++) {
-            auto found = record.find(nums[i]);
-            if (found != record.end())
-                return {found->second, i};
-            record.insert(unordered_map<int,int>::value_type(target - nums[i], i));
+        vector<int> ans;
+        map <int, int> hashMap;
+        map <int, int>::iterator iter;
+         
+        for (int i=0; i<nums.size();i++){
+            iter = hashMap.find(target-nums[i]);  
+             
+            if (iter != hashMap.end()){
+                ans.push_back(iter->second);
+                ans.push_back(i);
+                return ans;
+            }
+            else
+                hashMap.insert(pair<int,int>(nums[i], i)); 
         }
-        return {-1, -1};
+        return ans;
     }
 };
-
-int main(){
-  printf("%d",10);
-  return 0;
-}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### Leetcode practice
 > Hope to improve skill of technical interview.
 
-|Leetcode ID|Link to solution for cpp|Difficulty|Topic|
-|:---------:|:----------------------:|:--------:|:----:|
-|1|[Two Sum](https://github.com/Sinyu104/Leetcode-practice/blob/master/2-two_sum/solution.cpp)|Easy|Array|
+|Leetcode ID|Link to solution for cpp|Difficulty|Topic|Time compacity|Space sompacity|
+|:---------:|:----------------------:|:--------:|:----:|:-----------:|:-------------:|
+|1|[Two Sum](https://github.com/Sinyu104/Leetcode-practice/blob/master/2-two_sum/solution.cpp)|Easy|Array|O（n）|O（n）|
