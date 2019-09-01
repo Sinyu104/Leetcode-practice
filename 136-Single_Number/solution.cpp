fe67959cff1e36c26f2d6eaed0fc898e49ba4548
Sinyu104 <brandyhuang104@gmail.com>
2019-09-01 21:04:12 +0800

    add problem 136

---
+++ 136-Single_Number/solution.cpp
@@ -0,0 +1,22 @@
+#include "iostream"
+#include <map>
+#include <vector>
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int, int> num;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(num.find(nums[i])==num.end()){
+                sum+=nums[i];
+                num[nums[i]] =1;
+            }
+            else{
+                sum-=nums[i];
+            }
+            
+        }
+        return sum;
+    }
+};
